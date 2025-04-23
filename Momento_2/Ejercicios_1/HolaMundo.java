a48c2c458a867653100e9ddec2747eb0cb4dda99
BelfeGor-start <fsparra@ucompensar.edu.co>
2025-04-22 19:48:22 -0500

    Update HolaMundo.java

--- Momento_2/Ejercicios_1/HolaMundo.java
+++ Momento_2/Ejercicios_1/HolaMundo.java
@@ -2,6 +2,6 @@ package Algoritmos.ALGORITMOS_2025_1.Momento_2.Ejercicios_1;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo!"); // Este es un mensaje con print
     }
 }
